34ddf1326ee9dca726404b1baa3a172a037a5b5d
khanhtrinh107 <khanhtrinh107@gmail.com>
2025-03-24 21:50:12 +0700

    distributed-lock

--- src/main/java/com/example/demo/service/CacheService.java
+++ src/main/java/com/example/demo/service/CacheService.java
@@ -1,23 +1,51 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.AppException;
+import com.example.demo.exception.ErrorCode;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
 @Service
 @RequiredArgsConstructor
 public class CacheService {
     private final RedissonClient redissonClient;
+    private final ObjectMapper objectMapper;
+    private static long CACHE_TTL = 1;
 
-    public void saveToCache(String key, String value, long ttlInSeconds) {
-        RMapCache<String, String> mapCache = redissonClient.getMapCache("mycache");
-        mapCache.put(key, value);
-    }
+    public  <T> T getCacheAndLock(String cacheKey, TypeReference<T> typeRef, Supplier<T> dbFetcher) {
+        RMapCache<String, String> mapCache = redissonClient.getMapCache("attendanceCache");
+        try {
+            if (mapCache.containsKey(cacheKey)) {
+                return objectMapper.readValue(mapCache.get(cacheKey), typeRef);
+            }
+            String lockKey = "lock:" + cacheKey;
+            var lock = redissonClient.getLock(lockKey);
+            if (lock.tryLock(5, 10, TimeUnit.SECONDS)) {
+                if (mapCache.containsKey(cacheKey)) {
+                    return objectMapper.readValue(mapCache.get(cacheKey), typeRef);
+                }
+                T data = dbFetcher.get();
+                String jsonData = objectMapper.writeValueAsString(data);
+                mapCache.put(cacheKey, jsonData, CACHE_TTL, TimeUnit.DAYS);
 
-    public String getFromCache(String key) {
-        RMapCache<String, String> mapCache = redissonClient.getMapCache("mycache");
-        return mapCache.get(key);
+                return data;
+            } else {
+                Thread.sleep(100);
+                if (mapCache.containsKey(cacheKey)) {
+                    return objectMapper.readValue(mapCache.get(cacheKey), typeRef);
+                }
+            }
+        } catch (Exception e) {
+            throw new AppException(ErrorCode.UNCATEGORIZED_EXCEPTION);
+        }
+        throw new AppException(ErrorCode.UNCATEGORIZED_EXCEPTION);
     }
 
 }
